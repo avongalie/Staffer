c35759485492a1e47ade4928a5ef55588d0039d9
avongalie <alexsiaavila@gmail.com>
2022-05-26 17:56:47 -0400

    show message when they have no employees

--- src/components/employees/indexEmployees.js
+++ src/components/employees/indexEmployees.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { indexEmployees } from '../../api/employee'
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
 import './../../index.scss'
 
 class IndexEmployees extends Component {
@@ -17,7 +17,7 @@ class IndexEmployees extends Component {
     const { user } = this.props
     indexEmployees(user)
       .then((res) => res.data.employees.filter((employee) => employee.owner === this.props.user._id))
-      .then((res) => this.setState({ employees: res, show: true, owned: true }))
+      .then((res) => this.setState({ employees: res }))
       .catch(console.error)
   }
 
@@ -60,6 +60,15 @@ class IndexEmployees extends Component {
     const { employees } = this.state
     if (employees === null) {
       return 'Loading...'
+    } else if (employees.length === 0) {
+      return (
+        <>
+          <div style={{ textAlign: 'center' }}>
+            <h3 style={{ marginTop: '20px' }}>Please add an employee in the add employee tab!</h3>
+            <Button><Link style={{ textDecoration: 'none', color: 'white' }}to='/add-employee'>Add employee</Link></Button>
+          </div>
+        </>
+      )
     } else {
       const JSX = this.componentDidUpdate()
       return (
@@ -69,7 +78,7 @@ class IndexEmployees extends Component {
             <p>Search Employess by name below</p>
             <input id='search' name='search' onChange={this.handleChange}></input>
           </div>
-          <div>
+          <div className='row'>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
               {JSX}
             </div>
